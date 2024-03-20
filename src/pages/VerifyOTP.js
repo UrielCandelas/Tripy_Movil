dcf4b7e1f01f3c5091cf85c0919689eef973ca3a
ElliotFrias <elliotetian@gmail.com>
2024-03-19 23:46:36 -0600

    Front verificacion OTP

--- src/pages/VerifyOTP.js
+++ src/pages/VerifyOTP.js
@@ -1,15 +1,27 @@
-import { View, Text} from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native'
+import { React, useState } from 'react'
 import GeneralText from '../components/GeneralComponents/GeneralText'
 import GeneralInput from '../components/GeneralComponents/GeneralInput'
 import GeneralButton from '../components/GeneralComponents/GeneralButton'
+import useAuth from '../context/AuthContext'
 
 export default function VerifyOTP() {
+  const { verifyOTPFunc } = useAuth
+
+  const [OTP, setOTP] = useState('')
+
+  const handleSubmit = async () => {
+    console.log(OTP)
+    try {
+      const res = await verifyOTPFunc(OTP)
+    } catch (error) {}
+  }
+
   return (
     <View>
-      <GeneralText text = "Verify OTP" />
-      <GeneralInput/>
-      <GeneralButton/>
+      <GeneralText text="Verificar OTP" />
+      <GeneralInput onChangeText={setOTP} placeholder={'Ingresa OTP'} />
+      <GeneralButton text="Verificar" onPressHandler={handleSubmit} />
     </View>
   )
 }
